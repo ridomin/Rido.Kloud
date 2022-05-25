615b10b4162efb76dfd0fd91fc65a58e358ffc9f
ridomin <ridomin@live.com>
2022-05-24 18:24:31 -0700

    vue 1

---
+++ Rido.HubProxy/wwwroot/my-component.js
@@ -0,0 +1,25 @@
+﻿export default {
+    data: () => ({
+        devices: []
+    }),
+
+    created() {
+        this.fetchData()
+    },
+
+    methods: {
+        async fetchData() {
+            const url = `/devices/list`
+            this.devices = await (await fetch(url)).json()
+        },
+        async getStats(d) {
+            const url = `/pnp/${d.deviceId}/commands/getRuntimeStats`
+            try {
+                const resp = await (await fetch(url, { method: 'POST', body: 2, headers: { 'Content-Type': 'application/json' } })).json()
+                d.stats = JSON.stringify(resp, null, 2)
+            } catch {
+                d.stats = "Offline"
+            }
+        }
+    }
+}
