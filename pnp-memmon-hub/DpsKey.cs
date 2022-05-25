6c7b6fb66d6ec3ac6d6958444544f0db3305c40c
rido-min <rido-min@users.noreply.github.com>
2022-05-25 11:55:16 -0700

    add device

---
+++ pnp-memmon-hub/DpsKey.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace pnp_memmon_hub
+{
+    internal class DpsKey
+    {
+        internal static string ComputeDeviceKey(string masterKey, string deviceId)
+        {
+            using var hmac = new HMACSHA256(Convert.FromBase64String(masterKey));
+            return Convert.ToBase64String(hmac.ComputeHash(Encoding.UTF8.GetBytes(deviceId)));
+        }
+    }
+}
