a55294e1376dbdde75aa2e8fe44d7c51d522c60a
ridomin <ridomin@live.com>
2022-06-28 12:50:16 -0700

    start mqtt ux

---
+++ MqttUX/Controllers/DevicesController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rido.Mqtt.MqttNet4Adapter;
+using Rido.MqttCore;
+using System.Text.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MqttUX.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevicesController : ControllerBase
+    {
+        static Dictionary<string, Dictionary<string, object>> devices = new Dictionary<string, Dictionary<string, object>>();
+        IMqttBaseClient mqtt;
+        public DevicesController(IConfiguration config)
+        {
+            mqtt = new MqttNetClientConnectionFactory().CreateBasicClientAsync(new ConnectionSettings(config.GetConnectionString("broker"))).Result;
+            mqtt.OnMessage += async m =>
+            {
+                var segments = m.Topic.Split('/');
+                var did = segments[1];
+                if (devices.ContainsKey(did))
+                {
+                    devices.Remove(did);
+                }
+                devices.Add(did, JsonSerializer.Deserialize<Dictionary<string, object>>(m.Payload));
+                await Task.Yield();
+            };
+            mqtt.SubscribeAsync("pnp/+/birth").Wait();
+        }
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IDictionary<string, Dictionary<string, object>> Get()
+        {
+            return devices;
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MqttUX/wwwroot/index.js
@@ -0,0 +1,41 @@
+import * as creds from './creds.js'
+
+export default {
+    data: () => ({
+        config: '',
+        devices: [],
+        loading: true,
+        error: ''
+    }),
+    created() {
+        this.fetchData()
+    },
+    methods: {
+        async fetchData() {
+            const client = mqtt.connect(`wss://${creds.hostname}:8884/mqtt`, { 
+                clientId: creds.clientId, username: creds.username, password: creds.pwd })
+
+            client.on('error', e => console.error(e))
+            client.on('connect', () => {
+                console.log('connected', client.connected)
+                client.subscribe('pnp/+/birth')
+              })
+            client.on('message', (topic, message) => {
+                const msg = JSON.parse(message)
+                // console.log(topic, msg)
+                const deviceId = topic.split('/')[1]
+                const dix = this.devices.findIndex (d => d.deviceId === deviceId)
+                if (dix === -1) {
+                    this.devices.push({deviceId, status: msg.status, modelId: msg['model-id'], when: msg.when})
+                } else {
+                    this.devices[dix] = {deviceId, status: msg.status, modelId: msg['model-id'], when: msg.when}
+                }
+                this.devices.sort((a,b) => new Date(a.when) > new Date(b.when) )
+              })
+        },
+        formatDate(d) {
+            if (d === '0001-01-01T00:00:00Z') return ''
+            return moment(d).fromNow()
+        }
+    }
+}
