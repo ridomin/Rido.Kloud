2b399ad38c5801e10567dd3eb5af0c6a6a6b27e3
rido-min <rido-min@users.noreply.github.com>
2022-06-29 20:06:16 -0700

    delete

--- MqttUX/wwwroot/index.js
+++ MqttUX/wwwroot/index.js
@@ -1,5 +1,13 @@
 import * as creds from './creds.js'
 
+const client = mqtt.connect(`wss://${creds.hostname}:8884/mqtt`, { 
+    clientId: creds.clientId + Date.now(), username: creds.username, password: creds.pwd })
+
+const isBuffer = obj => {
+    return obj != null && obj.constructor != null &&
+    typeof obj.constructor.isBuffer === 'function' && obj.constructor.isBuffer(obj)
+}
+
 export default {
     data: () => ({
         hostname: '',
@@ -14,8 +22,6 @@ export default {
     methods: {
         async fetchData() {
             this.hostname = creds.hostname
-            const client = mqtt.connect(`wss://${creds.hostname}:8884/mqtt`, { 
-                clientId: creds.clientId, username: creds.username, password: creds.pwd })
 
             client.on('error', e => console.error(e))
             client.on('connect', () => {
@@ -23,17 +29,25 @@ export default {
                 client.subscribe('pnp/+/birth')
               })
             client.on('message', (topic, message) => {
-                const msg = JSON.parse(message)
-                // console.log(topic, msg)
                 const deviceId = topic.split('/')[1]
                 const dix = this.devices.findIndex (d => d.deviceId === deviceId)
-                if (dix === -1) {
-                    this.devices.push({deviceId, status: msg.status, modelId: msg['model-id'], when: msg.when})
-                } else {
-                    this.devices[dix] = {deviceId, status: msg.status, modelId: msg['model-id'], when: msg.when}
+                if (isBuffer(message) && message.byteLength > 0){
+                    const msg = JSON.parse(message)
+                    // console.log(topic, msg)
+                    if (dix === -1) {
+                        this.devices.push({deviceId, status: msg.status, modelId: msg['model-id'], when: msg.when})
+                    } else {
+                        this.devices[dix] = {deviceId, status: msg.status, modelId: msg['model-id'], when: msg.when}
+                    }
+                    this.devices.sort((a,b) => new Date(a.when) < new Date(b.when)  ? 1 : -1)
                 }
-                this.devices.sort((a,b) => new Date(a.when) < new Date(b.when)  ? 1 : -1)
               })
+        },
+        removeDevice(did) {
+            const topic = `pnp/${did}/birth`
+            client.publish(topic, '', {retain: true, qos:1})
+            const dix = this.devices.findIndex (d => d.deviceId === did)
+            this.devices.splice(dix,1)
         },
         formatDate(d) {
             if (d === '0001-01-01T00:00:00Z') return ''
