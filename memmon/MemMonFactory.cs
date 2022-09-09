af37ec7c7694a661fb55bfc0b369a0a56ebd689e
rido-min <rido-min@users.noreply.github.com>
2022-09-09 15:27:22 -0700

    memon fixes

--- memmon/MemMonFactory.cs
+++ memmon/MemMonFactory.cs
@@ -1,6 +1,7 @@
 ﻿using dtmi_rido_pnp_memmon;
 using MQTTnet;
 using MQTTnet.Client;
+using MQTTnet.Extensions.AwsClient;
 using MQTTnet.Extensions.Connections;
 using MQTTnet.Extensions.IoTHubClient;
 using MQTTnet.Extensions.PnPClient;
@@ -53,19 +54,18 @@ internal class MemMonFactory
     static async Task<dtmi_rido_pnp_memmon.mqtt.memmon> CreateBrokerClientAsync(string connectionString, CancellationToken cancellationToken = default)
     {
         var cs = new ConnectionSettings(connectionString) { ModelId = Imemmon.ModelId };
-        //var mqtt = new MqttFactory().CreateMqttClient(MqttNetTraceLogger.CreateTraceLogger()) as MqttClient;
-        //await mqtt.ConnectAsync(new MqttClientOptionsBuilder().WithConnectionSettings(cs, true).Build());
-        var mqtt = await BrokerClientFactory.CreatePnPBrokerClientAsync(cs, true, cancellationToken);
-        var client = new dtmi_rido_pnp_memmon.mqtt.memmon(mqtt.Connection);
+        
+        var mqtt = await BrokerClientFactory.CreateFromConnectionSettingsAsync(cs, true, cancellationToken);
+        var client = new dtmi_rido_pnp_memmon.mqtt.memmon(mqtt);
         return client;
     }
 
     static async Task<dtmi_rido_pnp_memmon.hub.memmon> CreateHubClientAsync(string connectionString, CancellationToken cancellationToken = default)
     {
         var cs = connectionString + ";ModelId=" + Imemmon.ModelId;
-        var hub = await HubDpsFactory.CreateFromConnectionStringAsync(cs);
+        var hub = await HubDpsFactory.CreateFromConnectionSettingsAsync(cs);
         connectionSettings = HubDpsFactory.ConnectionSettings;
-        var client = new dtmi_rido_pnp_memmon.hub.memmon(hub.Connection);
+        var client = new dtmi_rido_pnp_memmon.hub.memmon(hub);
         client.InitialState = await client.GetTwinAsync(cancellationToken);
         return client;
     }
@@ -73,8 +73,8 @@ internal class MemMonFactory
     static async Task<dtmi_rido_pnp_memmon.aws.memmon> CreateAwsClientAsync(string connectionString, CancellationToken cancellationToken = default)
     {
         var cs = new ConnectionSettings(connectionString) { ModelId = Imemmon.ModelId };
-        var mqtt = new MqttFactory().CreateMqttClient(MqttNetTraceLogger.CreateTraceLogger()) as MqttClient;
-        await mqtt.ConnectAsync(new MqttClientOptionsBuilder().WithConnectionSettings(cs).Build());
+        var mqtt = await AwsClientFactory.CreateFromConnectionSettingsAsync(connectionString, cancellationToken);
+        
         var client = new dtmi_rido_pnp_memmon.aws.memmon(mqtt);
         return client;
     }
